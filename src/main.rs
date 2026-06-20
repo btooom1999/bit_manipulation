eb251c65b0d3d4d98df3f4ef00d34927be677a3c
btooom1999 <btooom1999@gmail.com>
2026-06-20 15:55:02 +0700

    finished maximum_xor_after_operations exercise

--- src/main.rs
+++ src/main.rs
@@ -92,7 +92,8 @@ mod maximum_product_of_the_length_of_two_palindromic_subsequences;
 mod count_words_obtained_after_adding_a_letter;
 mod fair_distribution_of_cookies;
 mod maximum_rows_covered_by_columns;
+mod maximum_xor_after_operations;
 
 pub fn main() {
-    maximum_rows_covered_by_columns::main();
+    maximum_xor_after_operations::main();
 }

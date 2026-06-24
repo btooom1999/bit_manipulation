9dd83e2adc866a71632004bd0e70fa3178169aba
btooom1999 <btooom1999@gmail.com>
2026-06-24 13:23:49 +0700

    optimized maximum_strength_of_a_group exercise by kadane's algorithm

--- src/maximum_strength_of_a_group.rs
+++ src/maximum_strength_of_a_group.rs
@@ -1,37 +1,16 @@
 fn max_strength(nums: Vec<i32>) -> i64 {
-    if nums.len() == 1 {
-        return nums[0] as i64;
+    let (mut min, mut max) = (nums[0] as i64, nums[0] as i64);
+
+    for num in nums.into_iter().skip(1) {
+        let (temp_min, temp_max, num) = (min, max, num as i64);
+        max = num.max(max).max(temp_max * num).max(temp_min * num);
+        min = num.min(min).min(temp_min * num).min(temp_max * num);
     }
 
-    let (mut max, mut negative, mut product) = (i32::MIN, 0, 1i64);
-    let mut res = 0i64;
-    for num in nums {
-        if num < 0 {
-            negative += 1;
-            max = max.max(num);
-            product *= num as i64;
-        } else if num > 0 {
-            res = res.max(1) * num as i64;
-        }
-
-    }
-
-    if negative <= 1 {
-        return res;
-    }
-
-    if negative % 2 == 1 {
-        return res.max(1) * (product / max as i64);
-    }
-
-    if res == 0 {
-        return product;
-    }
-
-    res * product
+    max
 }
 
 pub fn main() {
-    let nums = [-2,-3].to_vec();
+    let nums = [-1,-2,-3,0].to_vec();
     println!("{}", max_strength(nums));
 }
